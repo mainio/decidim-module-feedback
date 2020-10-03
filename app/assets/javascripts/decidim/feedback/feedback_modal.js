a7086118bdf3eb86651fa400f95049ed5aa95f88
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-04 00:13:36 +0300

    Improve and simplify the feedback modal sending

--- app/assets/javascripts/decidim/feedback/feedback_modal.js
+++ app/assets/javascripts/decidim/feedback/feedback_modal.js
@@ -8,7 +8,20 @@
     const $cancelConfirm = $(".cancel-confirm", $modal);
     const $rating = $(".rating-field", $form);
 
+    const validateRating = () => {
+      const ratingValue = parseInt($("input:checked", $rating).val());
+      if (!ratingValue || ratingValue === 0) {
+        $("label", $rating).addClass("is-invalid-label");
+        $(".form-error-general", $rating).addClass("is-visible");
+        $form.data("form-valid", false);
+      } else {
+        $("label", $rating).removeClass("is-invalid-label");
+        $(".form-error-general", $rating).removeClass("is-visible");
+      }
+    };
+
     $modal.foundation("open");
+    $form.data("form-valid", false);
 
     $(".cancel-feedback", $modal).on("click.decidim-feedback", (ev) => {
       ev.preventDefault();
@@ -22,34 +35,21 @@
       $("input[type='radio']:first", $rating).focus();
     });
     if ($rating.length > 0) {
-      $("input", $rating).on("change", () => {
-        $("label", $rating).removeClass("is-invalid-label");
-        $(".form-error-general", $rating).removeClass("is-visible");
-      });
-      $("button[type='submit']", $form).on("click.decidim-feedback", (ev) => {
-        // Default form valid to true. If abide validation fails, it will be set
-        // to false which is checked before submission.
-        $form.data("form-valid", true);
-
-        const ratingValue = parseInt($("input:checked", $rating).val());
-        if (!ratingValue || ratingValue === 0) {
-          ev.preventDefault();
-          $("label", $rating).addClass("is-invalid-label");
-          $(".form-error-general", $rating).addClass("is-visible");
-        } else {
-          $("label", $rating).removeClass("is-invalid-label");
-          $(".form-error-general", $rating).removeClass("is-visible");
-        }
-      });
+      $("input", $rating).on("change", validateRating);
     }
 
+    $form.on("formvalid.zf.abide", () => {
+      $form.data("form-valid", true);
+      validateRating();
+    });
+    $form.on("forminvalid.zf.abide", () => {
+      $form.data("form-valid", false);
+      validateRating();
+    });
     // Rails does not understand the remote option in the forms that are in
     // reveal modals because they are re-entered into the DOM. Therefore, we
     // will prevent the default submit action and fire the submit through
     // rails-ujs.
-    $form.on("forminvalid.zf.abide", () => {
-      $form.data("form-valid", false);
-    });
     $form.on("submit.decidim-feedback", (ev) => {
       ev.preventDefault();
       if ($form.data("form-valid")) {
