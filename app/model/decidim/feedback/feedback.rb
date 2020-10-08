f79694855b5856854c140be689ca4bbe8758344e
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-10-08 17:49:02 +0300

    Show the contact request column in the feedbacks admin panel

--- app/model/decidim/feedback/feedback.rb
+++ app/model/decidim/feedback/feedback.rb
@@ -6,6 +6,11 @@ module Decidim
       belongs_to :organization, foreign_key: "decidim_organization_id", class_name: "Decidim::Organization"
       belongs_to :feedbackable, foreign_key: "decidim_feedbackable_id", foreign_type: "decidim_feedbackable_type", polymorphic: true
       belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
+
+      def contact_request_human
+        key = contact_request? ? "yes" : "no"
+        I18n.t("decidim.feedback.admin.models.feedbacks.values.contact_request.#{key}")
+      end
     end
   end
 end
