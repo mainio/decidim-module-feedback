8683e3597fee58e1ead875de1be63520612dab59
Joonas <joonas.aapro@mainiotech.fi>
2024-07-11 13:36:14 +0300

    Delete extra selenium spec fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,3 @@ Decidim::Dev.dummy_app_path =
   File.expand_path(File.join(__dir__, "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
-
-# This re-registration is made because of problems with chromedriver v120.
-# Selenium methods are undefined without this change.
-# See: https://github.com/decidim/decidim/pull/12160
-require "#{ENV.fetch("ENGINE_ROOT")}/lib/decidim/feedback/test/rspec_support/capybara"
