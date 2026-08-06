68a5c3e2bbd78c217fa3132606275d597eee31e2
Joonas <joonas.aapro@mainiotech.fi>
2026-08-06 08:15:22 +0300

    Update assets namespace

--- config/assets.rb
+++ config/assets.rb
@@ -2,12 +2,12 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs", prepend: true)
+Decidim::Shakapacker.register_path("#{base_path}/app/packs", prepend: true)
 
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_entrypoints(
   decidim_feedback: "#{base_path}/app/packs/entrypoints/decidim_feedback.js",
   decidim_feedback_admin: "#{base_path}/app/packs/entrypoints/decidim_feedback_admin.js",
   decidim_feedback_modal_admin: "#{base_path}/app/packs/entrypoints/decidim_feedback_modal_admin.js"
 )
 
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/feedback/feedback-form")
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/feedback/feedback-form")
